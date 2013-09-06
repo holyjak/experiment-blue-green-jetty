1a09ac524cef164eea7d12bee012937346bc01bf
Jakub Holy <jakub.holy@iterate.no>
2013-09-06 23:56:06 +0200

    Fixed ./simulate_clients.sh wrt new content

--- haproxy-vm/simulate_clients.sh
+++ haproxy-vm/simulate_clients.sh
@@ -4,16 +4,17 @@
 let blues=0
 let greens=0
 for i in $(seq -f '%02g' 1 50); do
-  wget localhost:8080 -O /tmp/html &> /tmp/wget.out
-  if grep --silent 'Env: blue' /tmp/html; then 
+  wget http://localhost:8080/js/deployment-bar.js -O webapp.out &> /tmp/wget.out
+  if grep --silent 'zone: blue' webapp.out; then 
     blues=$((blues+1))
-  elif grep --silent 'Env: green' /tmp/html; then 
+  elif grep --silent 'zone: green' webapp.out; then 
     greens=$((greens+1))
   else
-    echo "Neither blue nor green, an error perhaps? See /tmp/html"
+    echo "Neither blue nor green, an error perhaps? See ./webapp.out"
     echo "wget output:"
     cat /tmp/wget.out
     exit -1
   fi
+  rm webapp.out
 done
 echo ">> DONE; $blues served by blue, $greens served by green"
